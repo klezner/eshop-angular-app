94faf282394f5fa44c7040e56b600d18beda656d
Krzysztof Lezner <krzys3ek@gmail.com>
2021-02-20 14:51:47 +0100

    Add Product class

---
+++ src/app/common/product.ts
@@ -0,0 +1,12 @@
+export class Product {
+  sku: string;
+  name: string;
+  author: string;
+  description: string;
+  unitPrice: number;
+  imageUrl: string;
+  active: boolean;
+  unitsInStock: number;
+  dateCreated: Date;
+  lastUpdated: Date;
+}
